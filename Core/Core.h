4368f424fb0e85fb852c54b6f34fb3bad24bfea4
kilotron <cqycczq@126.com>
2019-03-14 16:38:37 +0800

    使用新算法改进无环查找的速度

--- Core/Core.h
+++ Core/Core.h
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <string.h>
 
+#define NUM_NODES 26
+
 class Core {
 public:
 	static int gen_chain_word(char* words[], int len, char* result[], char head, char tail, bool enable_loop);
@@ -18,7 +20,7 @@ struct Edge {
 	std::string *word;
 	int weight;
 	Node *to_node;
-	bool visited;
+	bool visited; // used in LongestPathFromDFS()
 	Edge(std::string * word, Node * to_node);
 };
 
@@ -26,6 +28,17 @@ struct Node {
 	char letter;
 	std::vector<Edge *> edges;
 	int node_idx;
+
+	// for topo sort
+	int indegree;
+
+	// path finding, initalize these variables before using.
+	int dist;
+	Edge *edgeToThis;
+	Node *pred;
+	bool selfLoop;
+	Edge *selfLoopEdge;
+
 	Node(char letter, int node_idx);
 };
 
@@ -43,15 +56,28 @@ private:
 	enum NodeStatus { NotExplored, BeingExplored, FullyExplored };
 
 	bool isCyclic;
-	Node *nodes[26]; // array
+	bool isWeightedGraph;
+	Node *nodes[NUM_NODES]; // array
 	int num_edges;
 
 	Node *GetNode(char letter);
 	void DetectCycle();
 	void DFS(int node_idx, int visited[]);
 
+	void TopoSort(std::vector<Node *> &result);
+	void SetWeight(bool isWeighted);
+
+	void MarkLongestPathFromAcyclic(char start);
+
+	Path * ConstructPath(Node * node);
+
 	void LongestPathBetweenDFS(char start, char end, std::vector<Edge*>& edgeStack, int &maxLen, Path **longestPath, bool isWeighted);
+	Path * LongestPathBetweenCyclicOrAcyclic(char start, char end, bool isWeighted);
 	void LongestPathFromDFS(char start, std::vector<Edge*>& edgeStack, int & maxLen, Path ** longestPath, bool isWeighted);
+	Path * LongestPathFromCyclicOrAcyclic(char start, bool isWeighted);
+
+	Path * LongestPathBetweenAcyclic(char start, char end, bool isWeighted);
+	Path * LongestPathFromAcyclic(char start, bool isWeighted);
 
 public:
 	WordGraph(char *words[], int len);
